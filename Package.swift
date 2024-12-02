6a541235edaa5a41ff58692948ecb648555c06af
Kabir Oberai <oberai.kabir@gmail.com>
2024-12-02 01:38:46 -0500

    Just packing

--- Package.swift
+++ Package.swift
@@ -10,20 +10,15 @@ let package = Package(
             name: "swiftpack",
             targets: ["Pack"]
         ),
-        .library(name: "SecuritySPI", targets: ["SecuritySPI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
-        .package(url: "https://github.com/apple/swift-certificates", from: "1.0.0-beta.1"),
     ],
     targets: [
-        .systemLibrary(name: "SecuritySPI"),
         .executableTarget(
             name: "Pack",
             dependencies: [
-                "SecuritySPI",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "X509", package: "swift-certificates")
             ]
         ),
     ]
